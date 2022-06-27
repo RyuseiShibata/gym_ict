240e390bd17bfaea4a2ea016180c18d8c9a3ca91
RyuseiShibata <67996465+RyuseiShibata@users.noreply.github.com>
2022-06-27 21:29:54 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ $(function () {
         var sex = $('input[name="sex"]:checked').val();
         var strength = $('input[name="strength"]selected').val();
       
-        var basal_metabolism
+        var basal_metabolism;
         
         if(sex=='men'){
           basal_metabolism = 13.397 * weight + 4.799 * height - 5.677 * age + 88.362;
