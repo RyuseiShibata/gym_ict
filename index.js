182cceb063e1f2a46abeb231c5e6be2b03e976eb
RyuseiShibata <67996465+RyuseiShibata@users.noreply.github.com>
2022-06-28 18:13:06 +0900

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ $(function () {
         var fat = diet_cal * 0.2 / 9 ;
         var carbohydrate = diet_cal * 0.4 / 4 ;
             
-        var msg = `【基礎代謝】\n ${basal_metabolism.toPrecision(4)}kcal\n\n 【メンテナンスカロリー】\n ${maintenance_cal.toPrecision(4)}kcal\n\n 【ダイエットカロリー】\n ${diet_cal.toPrecision(4)}kcal\n\n 【ダイエット時の栄養素】\n タンパク質：${protein.toPrecision(3)}g\n 脂質：${fat.toPrecision(2)}g\n 炭水化物：${carbohydrate.toPrecision(3)}g`;
+        var msg = `【基礎代謝】\n ${basal_metabolism.toPrecision(4)}kcal\n\n 【メンテナンスカロリー】\n ${maintenance_cal.toPrecision(4)}kcal\n\n 【ダイエットカロリー】\n ${diet_cal.toPrecision(4)}kcal\n\n 【ダイエット時の栄養素】\n タンパク質：${protein.toPrecision(3)}g\n 脂質：${fat.toPrecision(2)}g\n 炭水化物：${carbohydrate.toPrecision(3)}g\n\n※ハリス・ベネディクト方程式(改良版)を使って基礎代謝量を計算しています。\n※カロリー比でP:F:C=2:1:2で計算しています。`;
         sendText(msg);
 
         return false;
